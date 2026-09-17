3966f1c34cf1c079ceb5c5517f143c6f09be27cf
synnicaltech <synnicaltechnology@gmail.com>
2026-09-18 01:42:54 +0530

    webdriver framework integration (#29)

--- src/test/java/com/automation/login/LoginTest.java
+++ src/test/java/com/automation/login/LoginTest.java
@@ -1,6 +1,7 @@
 package com.automation.login;
 
 import com.automation.base.BaseTest;
+import com.automation.core.driver.DriverManager;
 import com.automation.pages.Login;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,9 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void openUrlTest(){
-        Login login = new Login();
+        Login login = new Login(DriverManager.getDriver());
         login.open();
         Assertions.assertTrue(login.getCurrentUrl().trim().contains("/auth/login"));
-        login.closeDriver();
     }
 
 }
