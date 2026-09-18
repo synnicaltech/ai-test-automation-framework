8f32ab5e451c36d6340f1511aa082f4e0d2ed657
synnicaltech <synnicaltechnology@gmail.com>
2026-09-18 20:40:18 +0530

    update login page with base page (#40)

--- src/main/java/com/automation/pages/Login.java
+++ src/main/java/com/automation/pages/Login.java
@@ -1,9 +1,16 @@
 package com.automation.pages;
 
 import com.automation.core.config.ConfigManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Login extends BasePage{
+
+    private final By usernameField = By.xpath("//input[@name='username']");
+
+    private final By passwordField = By.xpath("//input[@name='password']");
+
+    private final By loginButton = By.xpath("//button[contains(@class,'orangehrm-login-button') and @type='submit']");
 
     public Login(WebDriver driver) {
         super(driver);
@@ -12,6 +19,26 @@ public class Login extends BasePage{
     public Login open(){
         driver.get(ConfigManager.get("base.url"));
         return this;
+    }
+
+    public Login enterUsername(String usernameValue){
+        type(usernameField, usernameValue);
+        return this;
+    }
+
+    public Login enterPassword(String passwordValue){
+        type(passwordField, passwordValue);
+        return this;
+    }
+
+    public void clickLogin(){
+        click(loginButton);
+    }
+
+    public void login(String usernameValue, String passwordValue){
+        enterUsername(usernameValue);
+        enterPassword(passwordValue);
+        clickLogin();
     }
 
     public String getCurrentUrl(){
