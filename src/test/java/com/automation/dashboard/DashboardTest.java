bc625b4f1b087cc39e42decc3d050d32c555fd89
synnicaltech <synnicaltechnology@gmail.com>
2026-09-20 04:06:32 +0530

    completed component object modal (#53)

---
+++ src/test/java/com/automation/dashboard/DashboardTest.java
@@ -0,0 +1,20 @@
+package com.automation.dashboard;
+
+import com.automation.base.BaseTest;
+import com.automation.core.driver.DriverManager;
+import com.automation.pages.DashboardPage;
+import com.automation.pages.LoginPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DashboardTest extends BaseTest {
+
+    @Test
+    public void searchPIM(){
+        LoginPage loginPage = new LoginPage(DriverManager.getDriver()).open();
+        DashboardPage dashboardPage = loginPage.login("Admin", "admin123");
+        Assertions.assertTrue(dashboardPage.isDashboardPageDisplayed());
+        dashboardPage.sidebar().searchMenuItem("PIM").selectItem("PIM");
+        dashboardPage.header().openProfile().logout();
+    }
+}
