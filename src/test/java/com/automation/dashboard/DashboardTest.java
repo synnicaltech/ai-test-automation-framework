f014bc03b352f8e2f589369ba90c100d42098b89
synnicaltech <synnicaltechnology@gmail.com>
2026-09-21 00:43:12 +0530

    bind test data provider with test cases (#65)

--- src/test/java/com/automation/dashboard/DashboardTest.java
+++ src/test/java/com/automation/dashboard/DashboardTest.java
@@ -5,6 +5,8 @@ import com.automation.core.config.ConfigManager;
 import com.automation.core.driver.DriverManager;
 import com.automation.pages.DashboardPage;
 import com.automation.pages.LoginPage;
+import com.automation.testdata.model.LoginTestData;
+import com.automation.testdata.provider.LoginTestDataProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +14,9 @@ public class DashboardTest extends BaseTest {
 
     @Test
     public void searchPIM(){
+        LoginTestData data = LoginTestDataProvider.validLogin();
         LoginPage loginPage = new LoginPage(DriverManager.getDriver()).open(ConfigManager.get("base.url"));
-        DashboardPage dashboardPage = loginPage.login("Admin", "admin123");
+        DashboardPage dashboardPage = loginPage.login(data.username(), data.username());
         Assertions.assertTrue(dashboardPage.isDashboardPageDisplayed());
         dashboardPage.sidebar().searchMenuItem("PIM").selectItem("PIM");
         dashboardPage.header().openProfile().logout();
